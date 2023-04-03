464401bd3dcb8dd43c51aedc8e942a367c80b56a
Bruno Pontiz <bpontiz@gmail.com>
2023-04-03 12:43:57 -0300

    Cart module has been finished.

--- backend/src/controllers/usersController/usersController.js
+++ backend/src/controllers/usersController/usersController.js
@@ -20,6 +20,7 @@ usersController.authenticateUser = async (req, res) => {
     authenticateUser.message ?
         res.redirect('http://localhost:3000/logged/failed') :
         res.redirect('http://localhost:3000/logged');
+    return;
 };
 
 usersController.postUser = async (req, res) => {
@@ -27,24 +28,29 @@ usersController.postUser = async (req, res) => {
 
     if (user.password === user.passwordRepeated) {
         const addedUser = await addNew(user);
-        addedUser.message ? 
-            res.redirect('http://localhost:3000/registered/failed') : 
-            res.redirect('http://localhost:3000/registered/successful/');
+        console.log(addedUser);
+        !addedUser.message ? 
+            res.redirect('http://localhost:3000/registered/successful/') :
+            res.redirect('http://localhost:3000/registered/failed') ;
+        return;
     };
 
     res.redirect('http://localhost:3000/registered/passFailed');
+    return;
 };
 
 usersController.getUserById = async (req, res) => {
     const id = req.params._id;
     const getUser = await getOne(id);
     res.json(getUser);
+    return;
 };
 
 usersController.deleteUserById = async (req, res) => {
     const id = req.params._id;
     const deletedUser = await deleteOne(id);
     res.json(deletedUser);
+    return;
 };
 
 usersController.updateUserById = async (req, res) => {
@@ -52,6 +58,7 @@ usersController.updateUserById = async (req, res) => {
     const newUser = req.body;
     const updatedUser = await updateOne(id, newUser);
     res.json(updatedUser);
+    return;
 };
 
 export default usersController;
